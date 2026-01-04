dfe6dba0bb7e890fdfa7eaae3b73c5d2dad9846d
youngjaekwon <gczzga@gmail.com>
2026-01-05 03:56:13 +0900

    refactor: replace Daphne with uvicorn and add WhiteNoise for static files
    - ASGI 서버를 Daphne에서 uvicorn으로 변경
    - 정적 파일 서빙을 위해 WhiteNoise 미들웨어 추가
    - 개발 환경에서 collectstatic 없이 정적 파일 서빙 설정
    - Docker 설정에서 daphne 서비스 제거 및 uvicorn으로 통합

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 DJANGO_APPS = [
-    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -59,6 +58,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
